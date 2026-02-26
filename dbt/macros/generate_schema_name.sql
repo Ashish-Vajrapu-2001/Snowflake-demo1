a79d040daa7db975e4506b400ffe0bb38c8a89ab
Ashish-Vajrapu-2001 <Vajrapu.Ashish.20022001@gmail.com>
2026-02-26 18:52:58 +0530

    Add silver layer CDC pipeline code

---
+++ dbt/macros/generate_schema_name.sql
@@ -0,0 +1,9 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
+    {%- set prefix = target.name + '_' if target.name != 'prod' else '' -%}
+    {%- if custom_schema_name is none -%}
+        {{ prefix }}{{ default_schema }}
+    {%- else -%}
+        {{ prefix }}{{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
